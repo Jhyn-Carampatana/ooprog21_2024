005d917239798e0761e70f3dd99aac2eae41800b
Jhyn123 <152131858+Jhyn123@users.noreply.github.com>
2024-08-14 18:40:38 +0800

    Add files via upload

---
+++ Chapter 1/TableAndChairs.java
@@ -0,0 +1,14 @@
+class TableAndChairs {
+
+public static void main(String[] args){
+   
+   System.out.println("x               x");
+   System.out.println("x               x");
+   System.out.println("x    xxxxxxx    x");
+   System.out.println("xxxx x     x xxxx");   
+   System.out.println("x  x x     x x  x");
+   System.out.println("x  x x     x x  x");
+   
+   }
+   
+ }
