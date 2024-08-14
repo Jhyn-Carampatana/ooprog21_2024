95be5db0923c78d8ef16833acc3f313cdb07ef04
Jhyn123 <152131858+Jhyn123@users.noreply.github.com>
2024-08-14 19:00:44 +0800

    Add files via upload

---
+++ Chapter 1/MovieQuoteInfo.java
@@ -0,0 +1,12 @@
+public class MovieQuoteInfo {
+   public static void main (String[] args){
+   
+   System.out.println("Quote: I'll be back");
+   System.out.println("Movie: The Terminator");
+   System.out.println("Year: 1984");
+   System.out.println("Actor: Arnold Schwarzenegger");
+   
+   }
+
+}
+
