2928e998764576186dcae4b964168ba556c77920
Jhyn-Carampatana <152131858+Jhyn-Carampatana@users.noreply.github.com>
2024-08-21 18:23:28 +0800

    Add files via upload

---
+++ Chapter 1/SwingAlert.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+public class SwingAlert {
+JFrame f;
+SwingAlert() {
+f=new JFrame();
+  JOptionPane.showMessageDialog(f,"Hello Wolrd!");
+ 
+ }
+ public static void main(String[] args) {
+   new SwingAlert();
+ 
+ }
+}
