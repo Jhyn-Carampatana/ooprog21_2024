7f9ddf5e91c59d4bcecfc2fba74a961fc16b7afc
Jhyn-Carampatana <152131858+Jhyn-Carampatana@users.noreply.github.com>
2024-08-21 18:12:03 +0800

    Add files via upload

---
+++ Chapter 1/SwingAlert.java.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+public class OptionPaneExample {
+JFrame f;
+OptionPaneExample(){
+f=new JFrame();
+   JOptionPane.showMessageDialog(f,"Hello World!");
+ 
+ }
+ public static void main(String[] args){
+  new OptionPaneExample();
+ 
+ 
+ }
+}
