2008476276cdc276fcfea242bd38ad9f4a0f39e4
Jhyn-Carampatana <152131858+Jhyn-Carampatana@users.noreply.github.com>
2024-09-20 21:20:31 +0800

    Add files via upload

---
+++ Chapter 3/FirstAddressExercise.java
@@ -0,0 +1,11 @@
+public class FirstAddressExercise {
+   public static void main (String [] args) {
+        
+    displayAddress();
+   }
+   public static void displayAddress() {
+  System.out.println("Nokia Telecommunication Company\nNokia");
+      System.out.println( "Espoo, Finland");
+       
+      }
+     }
